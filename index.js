f4a5ea30022a87725fca47ddc39201afe02305e4
Annie1790 <budayaniko@outlook.com>
2022-11-26 09:24:50 +0000

    game works, design done

--- index.js
+++ index.js
@@ -1,54 +1,92 @@
-let myArray = ["Rock", "Paper", "Scissors"]; //creates an array 
+let myArray = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
-function getComputerChoice() { //returns a random string from the array
+function getComputerChoice() {
     let randomItem = myArray[Math.floor(Math.random() * myArray.length)];
     return randomItem
 }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) { //gives the scenarios
-        return ("tie")
+        return ("Tie!")
     } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        computerScore++;
         return ("You lose! Paper beats rock ")
     } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        playerScore++;
         return ("You win! Paper beats rock ")
     } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        computerScore++;
         return ("You lose! Rock beats scissors")
     } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        playerScore++;
         return ("You win! Rock beats scissors ")
     } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        playerScore++;
         return ("You win! Scissors beats Paper")
     } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        computerScore++;
         return ("You lose! Scissors beats paper ")
     } else {
-        return "Impossible choice"
+        return "check your code, something is wrong!"
     }
 }
 
-function playRoundDisplay(humanChoice, computerChoice) {
+function disableButton() {
+    const disBut = document.querySelectorAll(".button-flexbox > button");
+    for (let i = 0; i < disBut.length; i++) {
+        disBut[i].disabled = true
+    }
+}
+
+
+
+function game(humanChoice, computerChoice) {
+    const resultChoiceDisplay = document.querySelector("#result");
+    const newGame = document.querySelector("#new-game")
+    resultChoiceDisplay.textContent = playRound(humanChoice, computerChoice);
+    if (playerScore === 5) {
+        resultChoiceDisplay.textContent = "You Win!"
+        newGame.style.display = "flex"
+        disableButton();
+    } else if (computerScore === 5) {
+        resultChoiceDisplay.textContent = "You Lose!"
+        newGame.style.display = "flex"
+        disableButton();
+    }
+
     const humanChoiceDisplay = document.querySelector("#humanChoice");
     humanChoiceDisplay.textContent = humanChoice;
+    const playerScoreDisplay = document.querySelector("#humanScore");
+    playerScoreDisplay.textContent = playerScore
 
     const computerChoiceDisplay = document.querySelector("#computerChoice");
     computerChoiceDisplay.textContent = computerChoice;
+    const computerScoreDisplay = document.querySelector("#computerScore");
+    computerScoreDisplay.textContent = computerScore
 
-    const resultChoiceDisplay = document.querySelector("#result");
-    resultChoiceDisplay.textContent = playRound(humanChoice, computerChoice)
+
 }
 
 const humanChoiceRock = document.querySelector("#Rock");
 humanChoiceRock.addEventListener("click", function () {
-    playRoundDisplay("Rock", getComputerChoice())
+    game("Rock", getComputerChoice())
 })
 
 const humanChoicePaper = document.querySelector("#Paper");
 humanChoicePaper.addEventListener("click", function () {
-    playRoundDisplay("Paper", getComputerChoice())
+    game("Paper", getComputerChoice())
 })
 
 const humanChoiceScissors = document.querySelector("#Scissors");
 humanChoiceScissors.addEventListener("click", function () {
-    playRoundDisplay("Scissors", getComputerChoice())
+    game("Scissors", getComputerChoice())
 })
 
+const newGame = document.querySelector("#new-game")
+function reload() {
+    reload = location.reload()
+};
+newGame.addEventListener("click", reload, false);
 
